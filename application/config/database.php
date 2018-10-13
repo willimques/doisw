d5905855f89442c14c889d801c8d7dc53d257dfc
Willian Marques <lwo.marques@gmail.com>
2018-10-13 17:47:34 -0300

    configurações acesso

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
+	'hostname' => 'doisw.mysql.uhserver.com',
+	'username' => 'doisw',
+	'password' => 'Willian0510@',
 	'database' => 'sistemadoisw',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
